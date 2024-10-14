469ad1b08abf56dfd6161e0090d7e3be975a853a
Audiofool <146060063+Audiofool934@users.noreply.github.com>
2024-10-14 16:46:38 +0800

    update build

---
+++ build/CMakeFiles/patent_system.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/patent_system.dir/src/main.cpp.o"
+  "CMakeFiles/patent_system.dir/src/main.cpp.o.d"
+  "patent_system"
+  "patent_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/patent_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
